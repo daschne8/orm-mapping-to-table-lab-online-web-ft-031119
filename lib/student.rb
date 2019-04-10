ad4771eaa4d3eeb893b58c79758b115f244973d9
Daniel Schneider <daschne8@gmail.com>
2019-04-10 17:11:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,11 @@ class Student
   end
 
   def save
-
+    sql = <<-SQL
+    INSERT INTO students(name,grade)
+    VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql,self.name,self.grade)
   end
 
   def create
@@ -16,7 +20,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABEL IF NOT EXISTS songs (
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade TEXT
