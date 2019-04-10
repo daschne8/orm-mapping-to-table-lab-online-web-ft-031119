3f064a09e016a719a8d070cd79d682b0d655dd65
Daniel Schneider <daschne8@gmail.com>
2019-04-10 16:54:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 #  with DB[:conn]
 class Student
-  attr_accessor
+  attr_accessor :name,:grade,:id
 
-  def initialize()
-
+  def initialize(name,grade id = nil)
+    @name,@grade,@id = name,grade,id
   end
 
   def save
